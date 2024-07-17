8e1d036b46c3da2aa14044b5bbca2c40c9be3a1d
russelshane <hello@darsler.com>
2024-07-17 14:27:05 +0800

    feat: add header component

---
+++ src/components/custom/Header.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+function Header() {
+  return (
+    <header className="w-full shadow-sm p-8 flex justify-between items-center">
+      <img
+        className="object-contain max-w-52 md:max-w-72"
+        src="assets/branding/svg-gowander-logo-full-flex-dark.svg"
+        alt="GoWander"
+      />
+      <Button size="lg">Sign In</Button>
+    </header>
+  );
+}
+
+export default Header;
