550483b63fb45ff7e822edf167546d37701e0c1f
russelshane <hello@darsler.com>
2024-07-17 14:39:03 +0800

    fix: fix header logo size

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="w-full shadow-sm p-8 flex justify-between items-center">
       <img
-        className="object-contain max-w-52 md:max-w-72"
+        className="object-contain max-w-52 md:max-w-60"
         src="assets/branding/svg-gowander-logo-full-flex-dark.svg"
         alt="GoWander"
       />
