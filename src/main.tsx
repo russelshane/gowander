baa0ad7b18e93f54864d3a4dd2d58df459b3633b
russelshane <hello@darsler.com>
2024-07-17 14:26:24 +0800

    chore: setup routing with react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTrip from "./routes/create-trip/index.tsx";
+import Header from "@/components/custom/Header.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/trip/start",
+    element: <CreateTrip />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Header />
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
