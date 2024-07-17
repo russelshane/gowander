58a5b0bdc69eb8065939b46bf019c5df6e833039
russelshane <hello@darsler.com>
2024-07-17 18:57:54 +0800

    fix: decrease hero sections spacing

--- src/components/custom/Hero.tsx
+++ src/components/custom/Hero.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 function Hero() {
   return (
-    <div className="w-full max-w-4xl flex flex-col items-center mx-auto my-32 md:my-40 gap-9 p-6 md:p-0">
+    <div className="w-full max-w-4xl flex flex-col items-center mx-auto my-28 md:my-40 gap-9 p-6 md:p-0">
       <h1 className="w-full leading-tight font-extrabold text-center text-4xl  md:text-6xl md:leading-tight">
         Discover your next&nbsp;
         <span className="text-orange-300 inline-block">
