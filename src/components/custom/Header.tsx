896c1a97166050b2f6d348805f7b671a228a01f8
russelshane <hello@darsler.com>
2024-07-18 22:28:55 +0800

    feat: adds sticky and backdrop blur to header

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 function Header() {
   return (
-    <header className="w-full shadow-sm p-8 flex justify-between items-center">
+    <header className="w-full shadow-sm p-8 flex justify-between items-center backdrop-blur-md bg-white/30 sticky top-0 z-10">
       <a href="/">
         <img
           className="object-contain max-w-52 md:max-w-60"
