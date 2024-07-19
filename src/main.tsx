c43246819415d46bc8dc3d155c554590def76071
russelshane <hello@darsler.com>
2024-07-19 20:36:04 +0800

    chore: configure redux store for app state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CreateTrip from "@/routes/create-trip/index.tsx";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import CreateTrip from "@/routes/planner/create-trip";
 import App from "@/routes/App.tsx";
 import Header from "@/components/custom/Header.tsx";
 import Footer from "@/components/custom/Footer.tsx";
 import "@/index.css";
+import numberOfDaysReducer from "@/lib/plannerSlice";
 
+// Creates a global redux store to access state
+const store = configureStore({
+  reducer: {
+    plannerNumberOfDays: numberOfDaysReducer,
+  },
+});
+
+// Creates new app router handler with react-router-dom
 const router = createBrowserRouter([
   {
     path: "/",
@@ -20,8 +31,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Header />
-    <RouterProvider router={router} />
-    <Footer />
+    <Provider store={store}>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </Provider>
   </React.StrictMode>
 );
